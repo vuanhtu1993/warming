129c459cca3f66c89530113c4a1662b6f2dacd4c
tuva9 <tuva9@fpt.edu.vn>
2022-12-16 16:54:35 +0700

    update

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {updateActual, updateLoading} from './slices/weatherSlice'
+import {IActual} from './interfaces/weather'
+import {fetchWeather} from './api/weather'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { RootState } from './app/store'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const {actual} = useSelector((state: RootState) => state.weatherWatch)
+
+  function getWeather(content: string) {
+    dispatch(updateLoading(true));
+    fetchWeather(content).then(res => {
+      dispatch(updateActual(res.data as IActual));
+    }).catch(error => {
+      var msg = 'Cidade não encontrada!';
+
+      if (error.response.data.cod != 404) {
+        msg = error.response.data.message;
+      }
+      dispatch(updateActual({}));
+    }).finally(() => {
+      dispatch(updateLoading(false));
+    });
+  }
+
+  useEffect(function () {
+    getWeather("Ha noi")
+  }, [])
+
+  return (
+    <div className="App">
+      Weather app
+      {actual && JSON.stringify(actual.weather?.[0].main)}
+    </div>
+  )
+}
+
+export default App
